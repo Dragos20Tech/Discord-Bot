47d4a0d5d8898877eec34ae3c7d1d2464eacdfda
Dragos20Tech <dragospetrescu5@gmail.com>
2023-10-27 11:03:27 +0300

    Added .gitignore for security of classified info

--- apikeys.py
+++ apikeys.py
@@ -1,3 +1,3 @@
 
 # bot token from Discord Developer Portal (OAuth2 -> URL Generator)
-bot_token = 'MTE2MTIyOTI2NTMwODc0OTk1Nw.GLwn8Y.-SvrPxjGK3Q8_T4hx_eqxL1O0qM39zOGcVx8Mg'
+bot_token = 'YOUR_TOKEN'
